3a9d0787467bcfb45559636eb143c1282180ff10
RenatoPinheiro06 <renato_pinheiro06@hotmail.com>
2023-09-12 15:38:34 -0300

    primeiro progarama de calcular média

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'calcular-media',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
